4b5546f25d67fe895d3c51d3c05275805bee0f64
Yutong Zhang <54720958+Yutong-Z@users.noreply.github.com>
2022-11-24 20:14:25 +0000

    moved everything here

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # IDS706-Project4-Yutong
 This is the repo for IDS706 course project 4.
+
+OpenAI
+https://beta.openai.com/playground/p/default-qa?model=text-davinci-002
+
+### Usage
+
+TBA
+
+Project #4: Continuous Delivery of Flask/FastAPI Data Engineering API on AWS
+
+- Create a Microservice that returns a JSON payload and performs a Data Engineering related task
+- Push tested source code to Github and perform Continuous Integration with Github Actions (or similar SaaS Build service)
+- Configure Build Server to Deploy Changes on build (Continuous Delivery)
+- Create realistic API (reference here: Data Engineering: Chapter 5 aws chapter for pragmatic ai.)
---
+++ openai-app/app.py
@@ -0,0 +1,56 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    """Index function for Flask web app"""
+    if request.method == "POST":
+        question = request.form["question"]
+        # response1 = openai.Completion.create(
+        #     model="text-davinci-002",
+        #     prompt=generate_name_animal_prompt(question),
+        #     temperature=0.6,
+        # )
+        response2 = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=generate_qna_prompt(question),
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+        )
+        print(response2)
+        print(response2.choices[0].text)
+        return redirect(url_for("index", result=response2.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_name_animal_prompt(animal):
+    """Add two numbers"""
+    return """Suggest three names for an animal that is a superhero.
+    
+    Animal: Cat
+    Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+    Animal: Dog
+    Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+    Animal:""" + animal.capitalize() + "Names:"
+
+def generate_qna_prompt(question):
+    """Generate q & a prompt for OpenAI"""
+    return "I am a highly intelligent question answering bot. \
+        If you ask me a question that is rooted in truth, I will give you the answer. \
+        If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\n\
+        Q: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\n\
+        Q: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\n\
+        Q: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\n\
+        Q: How many squigs are in a bonk?\nA: Unknown\n\nQ:" + question + "\nA:"
